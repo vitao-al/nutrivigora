8fb937c79357da6ca799cff2a1aec922237eb361
vitao-al <vm74531@gmail.com>
2025-04-22 08:28:49 -0300

    kk

--- src/App.tsx
+++ src/App.tsx
@@ -218,7 +218,7 @@ function App() {
             {/* Logo and Search */}
             <div className="flex items-center flex-1 md:space-x-8">
               <img 
-                src="/logonv.png"  
+                src="/nvlogo.png"  
                 className="h-8"
               />
               <div className="hidden md:block flex-1 max-w-xl">
